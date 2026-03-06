0ed1ac0f08343af60a598c2bef88dfbf10d8c1d3
raghuvamshidharreddy <raghuvamshidharreddy@gmail.com>
2026-03-06 10:35:28 +0530

    changes

--- part4/page34.js
+++ part4/page34.js
@@ -3,7 +3,26 @@ const app=express();
 app.set("view engine",'ejs')
 app.set("views","./views")
 app.listen(8080,()=>console.log("Server Started"));
-app.get("/",(req,res)=>
+
+const users=[
+    {
+        name:"Revanth Reddy",
+        email:"randarevanth@gmail.com",
+        password:"password123"
+    },
+    {
+        name:"Praveen Nair",
+        email:"praveennair@gamil.com",
+        password:"praveenIsHandsome"
+
+    },
+    {
+        name:"Alice Johnson",
+        email:"alicejohnson@gmail.com",
+        password:"aliceIsCool"
+    }
+];
+app.get("/login",(req,res)=>
     {
     res.render("login");
 });
@@ -11,7 +30,7 @@ app.get("/register",(req,res)=>
     {
     res.render("register");
 });
-app.get("/dashboard",(req,res)=>
+app.get("/",(req,res)=>
     {
-    res.render("dashboard");
+    res.render("dashboard",{users});
 });
